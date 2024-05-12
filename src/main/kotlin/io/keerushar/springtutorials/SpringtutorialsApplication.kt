2260d687e8cde24b1d81abf205f4abc5cbe84283
Kiran Sharma <kiran.sharma@gurzu.com>
2024-05-12 13:03:21 +0545

    Initialized Project

---
+++ src/main/kotlin/io/keerushar/springtutorials/SpringtutorialsApplication.kt
@@ -0,0 +1,11 @@
+package io.keerushar.springtutorials
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringtutorialsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringtutorialsApplication>(*args)
+}
