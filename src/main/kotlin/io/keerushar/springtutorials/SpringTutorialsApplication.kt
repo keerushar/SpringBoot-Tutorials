684c551fff71a68295eab66f5ad8209244e70551
Kiran Sharma <kiran.sharma@gurzu.com>
2024-05-12 14:34:10 +0545

    chore: update changes

--- src/main/kotlin/io/keerushar/springtutorials/SpringTutorialsApplication.kt
+++ src/main/kotlin/io/keerushar/springtutorials/SpringTutorialsApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringtutorialsApplication
+class SpringTutorialsApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringtutorialsApplication>(*args)
+	runApplication<SpringTutorialsApplication>(*args)
 }
--- src/test/kotlin/io/keerushar/springtutorials/SpringTutorialsApplicationTests.kt
+++ src/test/kotlin/io/keerushar/springtutorials/SpringTutorialsApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringtutorialsApplicationTests {
+class SpringTutorialsApplicationTests {
 
 	@Test
 	fun contextLoads() {
