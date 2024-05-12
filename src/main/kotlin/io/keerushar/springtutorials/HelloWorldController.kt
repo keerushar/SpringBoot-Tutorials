2c038d89da877afd74a9c8701c0abbcc0230d51d
Kiran Sharma <kiran.sharma@gurzu.com>
2024-05-12 14:34:46 +0545

    chore: remove unused import

--- src/main/kotlin/io/keerushar/springtutorials/HelloWorldController.kt
+++ src/main/kotlin/io/keerushar/springtutorials/HelloWorldController.kt
@@ -1,6 +1,6 @@
 package io.keerushar.springtutorials
 
-import org.springframework.web.bind.annotation.GetMapping import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
