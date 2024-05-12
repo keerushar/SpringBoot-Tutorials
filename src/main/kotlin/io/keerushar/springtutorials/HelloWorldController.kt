ebbc1268c95b38e0e66e4160a974962dc62fc83e
Kiran Sharma <kiran.sharma@gurzu.com>
2024-05-12 14:33:59 +0545

    feat: create get api

---
+++ src/main/kotlin/io/keerushar/springtutorials/HelloWorldController.kt
@@ -0,0 +1,12 @@
+package io.keerushar.springtutorials
+
+import org.springframework.web.bind.annotation.GetMapping import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+    @GetMapping
+    fun hello() = "Hello Kiran, This is hello endpoint"
+}
